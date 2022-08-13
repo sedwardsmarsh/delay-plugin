62b63b2f28d6590501124c74d055e695c036413c
Samuel Edwards-Marsh <sam.david.marsh@gmail.com>
2022-08-13 15:38:13 -0700

    added gainSliderAttachment declaration. needs implementation

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -27,6 +27,11 @@ public:
 private:
     juce::Slider gainSlider;
     
+    // Need to create a slider attachment between our gain slider and the gain parameter.
+    // Our slider attachment must be destroyed before the slider object is destroyed:
+    // Classes in c++ are created from the top down, therefor we want to declare our slider attachment after our gainSlider.
+    std::unique_ptr<juce::AudioProcessorValueTreeState::SliderAttachment> gainSliderAttachment;
+    
     // This reference is provided as a quick way for your editor to
     // access the processor object that created it.
     NewProjectAudioProcessor& audioProcessor;
